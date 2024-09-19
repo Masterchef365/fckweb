e57ee5abe24867c47e3df9eb1cb1a1861e6ab4a8
Masterchef365 <duncan.freeman1@gmail.com>
2024-09-19 01:50:29 -0700

    Get some ideas down

--- framework/src/lib.rs
+++ framework/src/lib.rs
@@ -1,3 +1,5 @@
+use std::marker::PhantomData;
+
 use web_transport::Session;
 
 pub struct Framework {
@@ -10,9 +12,30 @@ unsafe impl Send for Framework {}
 
 impl Framework {
     pub fn new(sess: Session) -> Self {
-        Self {
-            sess
-        }
+        Self { sess }
     }
 }
 
+/// Which session, client or server did accept_bi()?
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(serde::Serialize, serde::Deserialize)]
+enum Accepter {
+    Client,
+    Server,
+}
+
+/// Uniquely identifies a stream, and carries type information about its contents.
+/// This is the type used to transmit information between client and server about the identity of a
+/// connected stream/sink combo. 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct BiStream<ClientToServer, ServerToClient> {
+    id: usize,
+    end: Acceptor,
+    _phantom: PhantomData<(ClientToServer, ServerToClient)>,
+}
+
+impl<CTS, STC> BiStream<CTS, STC> {
+    pub fn accept(&self, fr: &mut Framework) -> Box<dyn Transport> {
+    }
+}
