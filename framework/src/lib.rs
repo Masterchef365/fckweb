909d3c0e45fa1dcc1b0b41710774ed243c373582
Masterchef365 <duncan.freeman1@gmail.com>
2024-09-19 02:06:27 -0700

    moar

--- framework/src/lib.rs
+++ framework/src/lib.rs
@@ -1,9 +1,11 @@
 use std::marker::PhantomData;
 
+use futures::{Sink, Stream};
 use web_transport::Session;
 
 pub struct Framework {
     pub sess: Session,
+    pub next_id: usize,
 }
 
 /// Don't worry about it
@@ -12,30 +14,28 @@ unsafe impl Send for Framework {}
 
 impl Framework {
     pub fn new(sess: Session) -> Self {
-        Self { sess }
+        Self { sess, next_id: 0 }
     }
-}
 
-/// Which session, client or server did accept_bi()?
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-#[derive(serde::Serialize, serde::Deserialize)]
-enum Accepter {
-    Client,
-    Server,
+    fn get_next_id(&mut self) -> usize {
+        let next = self.next_id + 1;
+        std::mem::replace(&mut self.next_id, next)
+    }
 }
 
 /// Uniquely identifies a stream, and carries type information about its contents.
 /// This is the type used to transmit information between client and server about the identity of a
-/// connected stream/sink combo. 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-#[derive(serde::Serialize, serde::Deserialize)]
+/// connected stream/sink combo.
+///
+/// This is a type you should return from your API, in order to get a bidirectional stream on the other end.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct BiStream<ClientToServer, ServerToClient> {
     id: usize,
-    end: Acceptor,
     _phantom: PhantomData<(ClientToServer, ServerToClient)>,
 }
 
 impl<CTS, STC> BiStream<CTS, STC> {
-    pub fn accept(&self, fr: &mut Framework) -> Box<dyn Transport> {
+    pub async fn accept(&self, fr: &mut Framework) -> Box<dyn Stream<CTS> + Sink<STC>> {
+        todo!()
     }
 }
