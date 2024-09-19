1d7119e120047773d8cacf2eda28b85d1a203578
Masterchef365 <duncan.freeman1@gmail.com>
2024-09-18 23:38:04 -0700

    No this doesn't work either. Heck

--- quic-session/src/lib.rs
+++ quic-session/src/lib.rs
@@ -9,8 +9,14 @@ pub use web_transport;
 
 #[cfg(target_arch = "wasm32")]
 pub async fn client_session(url: &Url) -> Result<web_transport::Session> {
+    let hexes = include_bytes!("certs/localhost.hex");
+    let hexes: Vec<u8> = hexes
+        .chunks_exact(2)
+        .map(|chunk| u8::from_str_radix(&String::from_utf8(chunk.to_vec()).unwrap(), 16).unwrap())
+        .collect();
+
     Ok(web_transport_wasm::SessionBuilder::new(url.clone())
-        .server_certificate_hashes(vec![include_bytes!("certs/localhost.crt").to_vec()])
+        .server_certificate_hashes(vec![hexes])
         .connect()
         .await
         .map_err(|e| anyhow::format_err!("{e}"))?
