a94314cb6ee7e7a0078e580b118e7b905cd4da97
Masterchef365 <duncan.freeman1@gmail.com>
2024-09-20 18:36:55 -0700

    WIP

--- framework/src/lib.rs
+++ framework/src/lib.rs
@@ -1,10 +1,12 @@
 pub use tarpc;
 pub use serde;
-use std::marker::PhantomData;
+use tarpc::{transport::channel::UnboundedChannel, Transport};
+use tokio::io::{AsyncWriteExt, DuplexStream, SimplexStream, ReadHalf, WriteHalf};
+use std::{marker::PhantomData, sync::Arc, task::Poll};
 use serde::{Serialize, Deserialize, de::DeserializeOwned};
 
-use futures::{Sink, Stream};
-use web_transport::Session;
+use futures::{AsyncRead, Sink, SinkExt, Stream, StreamExt};
+use web_transport::{RecvStream, SendStream, Session};
 
 pub struct Framework {
     pub sess: Session,
@@ -49,15 +51,73 @@ impl<CTS, STC> TypedBiStream<CTS, STC> {
 }
 */
 
+const BUFFER_SIZE: usize = 4096; // Chosen arbitrarily!
+const MAX_READ_BYTES: usize = 4096; // Chosen arbitrarily!
+
 /// This is the type used to provide connectivity to an alternate tarpc connection
 #[derive(Clone, Copy, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct TarpcBiStream(BiStream);
 
+pub fn webtransport_rx_stream(mut rx: web_transport::RecvStream) -> ReadHalf<tokio::io::SimplexStream> {
+    let (ret, mut proxy) = tokio::io::simplex(BUFFER_SIZE);
 
+    tokio::spawn(async move {
+        loop {
+            if let Some(bytes) = rx.read(MAX_READ_BYTES).await? {
+                proxy.write(bytes.as_ref()).await?;
+            }
+        }
+
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
+    });
+
+    ret
+}
+
+/*
+pub fn webtransport_futures_bridge<Rx, Tx>((mut rx, mut tx): (RecvStream, SendStream)) -> DuplexStream {
+    let (duplex, returned) = tokio::io::duplex(BUFFER_SIZE);
+
+    let duplex = Arc::new(tokio::sync::Mutex::new(duplex));
+
+    let dup1 = duplex.clone();
+
+    tokio::spawn(async move {
+        loop {
+            if let Some(bytes) = rx.read(MAX_READ_BYTES).await? {
+                dup1.lock().await.write(bytes.as_ref()).await?;
+            }
+        }
+
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
+    });
+
+    tokio::spawn(async move {
+        loop {
+            if let Some(bytes) = rx.read(MAX_READ_BYTES).await? {
+                dup1.lock().await.write(bytes.as_ref()).await?;
+            }
+        }
+
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
+    });
+
+
+    returned
+}
+*/
+
+/// The encoding function for all data. Mostly for internal use, exposed here for debugging
+/// potential
 pub fn encode<T: Serialize>(value: &T) -> bincode::Result<Vec<u8>> {
     bincode::serialize(value)
 }
 
+/// The dencoding function for all data. Mostly for internal use, exposed here for debugging
+/// potential
 pub fn decode<T: DeserializeOwned>(bytes: &[u8]) -> bincode::Result<T> {
     bincode::deserialize(bytes)
 }
