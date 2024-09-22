7004ba07bf42e53644f372a392df743621f8fe7f
Masterchef365 <duncan.freeman1@gmail.com>
2024-09-21 20:31:57 -0700

    Some bones

--- framework/src/lib.rs
+++ framework/src/lib.rs
@@ -1,6 +1,8 @@
+use futures::Future;
+
+pub use futures;
 pub use serde;
 pub use tarpc;
-pub use futures;
 
 use bytes::Bytes;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
@@ -33,4 +35,24 @@ impl Framework {
         let next = self.next_id + 1;
         std::mem::replace(&mut self.next_id, next)
     }
+}
+
+// NOTE: These have different trait requirements. This is an unfortunate consequence of
+// incompatability of many libraries we are using between WASM and native, papered over by these functions.
+
+#[cfg(target_arch = "wasm32")]
+pub fn spawn<F>(future: F)
+where
+    F: Future<Output = ()> + 'static,
+{
+    wasm_bindgen_futures::spawn_local(future);
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn spawn<F>(future: F)
+where
+    F: Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    tokio::spawn(future);
 }
