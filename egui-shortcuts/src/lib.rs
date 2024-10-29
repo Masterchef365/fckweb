a261d4845e0050699f206a33a0b3217e7bcb80fa
Masterchef365 <duncan.freeman1@gmail.com>
2024-10-28 18:35:20 -0700

    Make Promises

--- egui-shortcuts/src/lib.rs
+++ egui-shortcuts/src/lib.rs
@@ -1,2 +1,21 @@
 mod spawner;
+pub use poll_promise::Promise;
 pub use spawner::SimpleSpawner;
+pub use std::future::Future;
+
+#[cfg(target_arch = "wasm32")]
+pub fn spawn_promise<F>(fut: F) -> Promise<F::Output>
+where
+    F: Future<Output = T> + 'static,
+{
+    Promise::spawn_local(fut)
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn spawn_promise<F>(fut: F) -> Promise<F::Output>
+where
+    F: Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    Promise::spawn_async(fut)
+}
