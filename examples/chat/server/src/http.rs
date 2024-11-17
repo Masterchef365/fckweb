8a1c5b63490be8c00f18f9619a73defa423b4e2e
Masterchef365 <duncan.freeman1@gmail.com>
2024-11-17 03:59:32 -0800

    Fix client dir

--- examples/chat/server/src/http.rs
+++ examples/chat/server/src/http.rs
@@ -5,7 +5,7 @@ use std::net::SocketAddr;
 use tower_http::services::ServeDir;
 
 pub async fn http_server() {
-    let server = ServeDir::new("../../client/dist/");
+    let server = ServeDir::new("../client/dist/");
     let app = Router::new().nest_service("/", server);
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
